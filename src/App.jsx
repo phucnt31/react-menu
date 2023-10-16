9bafa5fa299e01ca067b8637f307330bd70358a7
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-16 20:49:47 +0700

    Setup state value and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import Title from "./Title";
+import menu from "./data";
+import Menu from "./Menu";
 
 const App = () => {
+  const [menus, setMenus] = useState(menu);
+
   return (
     <main className="menu">
       <Title text="our menu" />
+      <Menu menus={menus} />
     </main>
   );
 };
