0da01e679ce358ae3c57cb99f859922282d15da9
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-17 09:44:13 +0700

    Create state to store categories and passed down to Categories.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import { useState } from "react";
 import Title from "./Title";
 import menu from "./data";
 import Menu from "./Menu";
+import Categories from "./Categories";
 
 const App = () => {
   const [menus, setMenus] = useState(menu);
+  const [categories, setCategories] = useState([
+    "all",
+    ...new Set(menu.map((item) => item.category)),
+  ]);
 
   return (
     <main className="menu">
       <Title text="our menu" />
       <Menu menus={menus} />
+      <Categories categories={categories} />
     </main>
   );
 };
